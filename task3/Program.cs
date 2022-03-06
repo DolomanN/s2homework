4246f82446a9baf8cb9663b928344d8ec7b08f2e
Nikita <nikitya39@gmail.com>
2022-03-06 21:56:10 +0200

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = 7;
+int number3 = 23;
+
+if (number1 % number2 == 0 & number1 % number3 == 0)
+{
+    Console.WriteLine("Кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine("Не кратно");
+}
