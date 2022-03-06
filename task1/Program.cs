da1fb2c7b67622776ff3598793bbb4ca83392af6
Nikita <nikitya39@gmail.com>
2022-03-06 20:49:34 +0200

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+string numbers = Convert.ToString(Console.ReadLine());
+int[] array = new int[numbers.Length];
+for (int i=0; i < numbers.Length; i++)
+{
+array[i] = int.Parse(numbers[i].ToString());
+}
+
+Console.WriteLine("Введите порядковый номер отображаемого элемента числа: ");
+int index = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine (array[index-1]);
